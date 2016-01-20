ab31544a1239ea70142dd03524116ebb1d60e83f
Kamil <m.kowalski767@wp.pl>
2016-01-20 22:25:27 +0100

    orw7

---
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MathsComponentNameSpace;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            MathsComponent obj = new MathsComponent();
+            int result = obj.Add(2, 2);
+            Assert.AreEqual(4, result);
+        }
+    }
+}
